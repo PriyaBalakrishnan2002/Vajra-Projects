69151feebe450dad689e51473b0b081d1bb4ec5b
KarthigaiPriya Balakrishnan <131988892+PriyaBalakrishnan2002@users.noreply.github.com>
2024-01-10 19:57:13 +0530

    Add files via upload

---
+++ Javascript/example.js
@@ -0,0 +1,14 @@
+
+//function for myFunction2
+function myFunction2() {
+    var pass = document.getElementById("myPass"); 
+    window.alert("Login by student! password"+" "+pass.value);
+    
+}
+//function for myFunction1
+function myFunction1(){
+    var pass = document.getElementById("myPass"); 
+    window.alert("Login by staff! password"+" "+pass.value);
+}   
+
+
